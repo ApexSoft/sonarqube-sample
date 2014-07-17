1e2813380d87715c1b88eadab62e257d43a11e36
go2zo <go2zo@apexsoft.co.kr>
2014-07-17 17:21:08 +0900

    modify source code

--- sample/src/main/java/IntegerArray.java
+++ sample/src/main/java/IntegerArray.java
@@ -3,88 +3,123 @@
  */
 public class IntegerArray {
 
-    public final static int BUBBLE_SORT = 1;
-    public final static int QUICK_SORT = 2;
-    public final static int MERGE_SORT = 3;
+	public final static int BUBBLE_SORT = 1;
+	public final static int QUICK_SORT = 2;
+	public final static int MERGE_SORT = 3;
 
-    private int[] numbers;
-    private int size;
+	private int[] numbers;
+	private int size;
+	private int[] helper;
 
-    public IntegerArray(int[] values) {
-        this.numbers = values;
-        this.size = numbers.length;
-    }
+	public IntegerArray(int[] values) {
+		this.numbers = values;
+		this.size = numbers.length;
+	}
 
-    public void sort(int algorithmType) {
-        switch (algorithmType) {
-            case BUBBLE_SORT:
-                bubbleSort();
-                break;
-            case QUICK_SORT:
-                quickSort(0, size - 1);
-                break;
-            case MERGE_SORT:
-                mergeSort();
-        }
-    }
+	public void sort(int algorithmType) {
+		switch (algorithmType) {
+		case BUBBLE_SORT:
+			bubbleSort();
+			break;
+		case QUICK_SORT:
+			quickSort(0, size - 1);
+			break;
+		case MERGE_SORT:
+			helper = new int[size];
+			mergeSort(0, size - 1);
+		}
+	}
 
-    private void bubbleSort() {
-        int low = 0;
-        int high = size - 1;
+	private void bubbleSort() {
+		int low = 0;
+		int high = size - 1;
 
-        while (low < high) {
-            int lowest  = low;
-            int highest = high;
-            low = size;
-            for (int i = lowest; i < highest; i++) {
-                if (numbers[i] > numbers[i + 1]) {
-                    exchange(i, i + 1);
-                    if (i < low) {
-                        low = i-1;
-                        if (low < 0) {
-                            low = 0;
-                        }
-                    } else if (i > high) {
-                        high = i + 1;
-                    }
-                }
-            }
-        }
-    }
+		while (low < high) {
+			int lowest  = low;
+			int highest = high;
+			low = size;
+			for (int i = lowest; i < highest; i++) {
+				if (numbers[i] > numbers[i + 1]) {
+					exchange(i, i + 1);
+					if (i < low) {
+						low = i-1;
+						if (low < 0) {
+							low = 0;
+						}
+					} else if (i > high) {
+						high = i + 1;
+					}
+				}
+			}
+		}
+	}
 
-    private void quickSort(int low, int high) {
-        int i = low, j = high;
-        int pivot = numbers[low + (high - low) / 2];
+	private void quickSort(int low, int high) {
+		int i = low, j = high;
+		int pivot = numbers[low + (high - low) / 2];
 
-        while (i <= j) {
-            while (numbers[i] < pivot) {
-                i++;
-            }
-            while (numbers[j] > pivot) {
-                j--;
-            }
-            if (i <= j) {
-                exchange(i, j);
-                i++;
-                j--;
-            }
-        }
+		while (i <= j) {
+			while (numbers[i] < pivot) {
+				i++;
+			}
+			while (numbers[j] > pivot) {
+				j--;
+			}
+			if (i <= j) {
+				exchange(i, j);
+				i++;
+				j--;
+			}
+		}
 
-        if (low < j) {
-            quickSort(low, j);
-        }
-        if (i < high) {
-            quickSort(i, high);
-        }
-    }
+		if (low < j) {
+			quickSort(low, j);
+		}
+		if (i < high) {
+			quickSort(i, high);
+		}
+	}
 
-    private void mergeSort() {
-        
-    }
+	private void mergeSort(int low, int high) {
+		if (low < high) {
+			int middle = low + (high - low) / 2;
+			mergeSort(low, middle);
+			mergeSort(middle + 1, high);
+			merge(low, middle, high);
+		}
+	}
 
-    private void exchange(int i, int j) {
-        numbers[i] = numbers[i] ^ numbers[j];
-        numbers[j] = numbers[i] ^ numbers[j];
-        numbers[i] = numbers[i] ^ numbers[j];
-    }
+	private void merge(int low, int middle, int high) {
+		for (int i = low; i <= high; i++) {
+			helper[i] = numbers[i];
+		}
+
+		int i = low;
+		int j = middle + 1;
+		int k = low;
+		while (i <= middle && j <= high) {
+			if (helper[i] <= helper[j]) {
+				numbers[k] = helper[i];
+				i++;
+			} else {
+				numbers[k] = helper[j];
+				j++;
+			}
+			k++;
+		}
+		while (i <= middle) {
+			numbers[k] = helper[i];
+			k++;
+			i++;
+		}
+	}
+
+	private void exchange(int i, int j) {
+		if (i == j) {
+			return;
+		}
+		numbers[i] = numbers[i] ^ numbers[j];
+		numbers[j] = numbers[i] ^ numbers[j];
+		numbers[i] = numbers[i] ^ numbers[j];
+	}
 }
--- sample/src/test/java/IntegerArrayTest.java
+++ sample/src/test/java/IntegerArrayTest.java
@@ -8,47 +8,71 @@ import java.util.Random;
  */
 public class IntegerArrayTest {
 
-    private int[] numbers;
-    private final static int SIZE = 9;
-    private final static int MAX = 100;
+	private int[] numbers;
+	private final static int SIZE = 9;
+	private final static int MAX = 100;
 
-    @Before
-    public void setup() throws Exception {
-        numbers = new int[SIZE];
-        Random generator = new Random();
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = generator.nextInt(MAX);
-        }
-    }
+	@Before
+	public void setup() throws Exception {
+		numbers = new int[SIZE];
+		Random generator = new Random();
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = generator.nextInt(MAX);
+		}
+	}
 
-    @Test
-    public void testBubbleSort() {
-        IntegerArray sorter = new IntegerArray(numbers);
-        sorter.sort(IntegerArray.BUBBLE_SORT);
-        if (!validate(numbers)) {
-            printResult(numbers);
-        }
-    }
+	@Test
+	public void testBubbleSort() {
+		System.out.println("**Bubble Sort**");
+		System.out.print("before: ");
+		printResult(numbers);
+		IntegerArray sorter = new IntegerArray(numbers);
+		sorter.sort(IntegerArray.BUBBLE_SORT);
+		if (validate(numbers)) {
+			System.out.print("sorted: ");
+			printResult(numbers);
+		}
+	}
 
-    @Test
-    public void testQuickSort() {
-        IntegerArray sorter = new IntegerArray(numbers);
-        sorter.sort(IntegerArray.QUICK_SORT);
-    }
+	@Test
+	public void testQuickSort() {
+		System.out.println("**Quick Sort**");
+		System.out.print("before: ");
+		printResult(numbers);
+		IntegerArray sorter = new IntegerArray(numbers);
+		sorter.sort(IntegerArray.QUICK_SORT);
+		if (validate(numbers)) {
+			System.out.print("sorted: ");
+			printResult(numbers);
+		}
+	}
 
-    private boolean validate(int[] numbers) {
-        for (int i = 0; i < numbers.length - 1; i++) {
-            if (numbers[i] > numbers[i + 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
+	@Test
+	public void testMergeSort() {
+		System.out.println("**Merge Sort**");
+		System.out.print("before: ");
+		printResult(numbers);
+		IntegerArray sorter = new IntegerArray(numbers);
+		sorter.sort(IntegerArray.MERGE_SORT);
+		if (validate(numbers)) {
+			System.out.print("sorted: ");
+			printResult(numbers);
+		}
+	}
 
-    private void printResult(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i]);
-        }
-        System.out.println();
-    }
+	private boolean validate(int[] numbers) {
+		for (int i = 0; i < numbers.length - 1; i++) {
+			if (numbers[i] > numbers[i + 1]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private void printResult(int[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.print(numbers[i] + " ");
+		}
+		System.out.println();
+	}
 }
